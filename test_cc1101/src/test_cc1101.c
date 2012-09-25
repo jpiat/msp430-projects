f20bf9fb80f81247c5e527142df10b04f8d47e16
jpiat <jpiat@jpiat-laptop.(none)>
2012-09-25 09:46:46 +0200

    adde main file

--- test_cc1101/src/test_cc1101.c
+++ test_cc1101/src/test_cc1101.c
@@ -1,23 +1,38 @@
 #include <msp430g2553.h>
-#include "cc1101.h"
+#include "CC1101_simple_link_reg_config.h"
+#include "uart.h"
 
+
+
+void uart_rx(unsigned char val){
+	uchar stat ;		
+		if(val == '\n'){
+			stat = strobe_cc1101(CC1101_SFRX);			
+			uart_send(stat);	
+		}else{
+			stat = write_cc1101_reg(CC1101_TXFIFO, val);			
+			uart_send(stat);	
+		}
+}
 
 int main(){
-	uchar tx_buffer [64] ;
-	uint i ;	
+	uchar fifo_count, tx_char ;	
 	WDTCTL = WDTPW + WDTHOLD ;
 	BCSCTL1 = CALBC1_16MHZ ;
 	DCOCTL = CALDCO_16MHZ ;
 	setup_cc1101_spi();	
-	setup_cc1101(cc1101_433_cfg, 2);	
+	setup_cc1101(cc1101_cfg, 22);
+	setup_uart_9600();
 	P1DIR |= BIT0 ;
 	P1OUT &= ~BIT0 ;
-	for(i = 0 ; i < 64 ; i ++){
-		tx_buffer[i] = i ;	
-	}
+	__bis_SR_register(GIE);
 	while(1){
 		P1OUT ^= BIT0 ;
-		write_cc1101_buffer(CC1101_TXFIFO , tx_buffer, NULL, 2);
-		__delay_cycles(100000);		
+		__delay_cycles(100000);	
+		read_cc1101_reg(CC1101_RXBYTES, &fifo_count);
+		if(fifo_count & 0x7F){
+			read_cc1101_reg(CC1101_RXFIFO, &tx_char);
+			uart_send(tx_char);
+		}
 	}
 }
