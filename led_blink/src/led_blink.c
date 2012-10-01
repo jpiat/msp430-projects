b6cac49749153c6e68e4d1a0b27a4c3dba08e29e
jpiat <jpiat@jpiat-laptop.(none)>
2012-10-01 14:51:41 +0200

    improving led blink example

--- led_blink/src/led_blink.c
+++ led_blink/src/led_blink.c
@@ -1,11 +1,21 @@
 #include <msp430g2553.h>
 
+unsigned char modulo ;
+
 int main(){
 	WDTCTL = WDTPW + WDTHOLD ;
-	P1DIR |= BIT0 ;
-	P1OUT &= ~BIT0 ;
+	P1DIR |= BIT0 | BIT6;
+	P1OUT &= ~ (BIT0 | BIT6) ;
+	modulo = 0 ;
 	while(1){
 		P1OUT ^= BIT0 ;
+		if(modulo & 0x08){
+			 P1OUT |= BIT6 ;
+		}
+		else{
+			P1OUT &= ~BIT6 ;
+		}
+		modulo ++ ;
 		__delay_cycles(100000);		
 	}
 }
