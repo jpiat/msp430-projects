f7cb5586fa578ea32c2a7707283aff93de2c15d8
jpiat <jpiat@jpiat-laptop.(none)>
2012-11-25 19:47:58 +0100

    show card on uart

--- MF522/src/mf522_test.c
+++ MF522/src/mf522_test.c
@@ -9,17 +9,21 @@ spi_slave mf522_spi ;
 uchar buffer[24] ;
 char printBuffer[16];
 
-void uart_rx(){
+char * msg1 = "no card detected\n";
+
+void uart_rx(unsigned char byte){
 	return ;
 }
 
 
 int main(){
-	uchar dummy, index, status;
+	uchar status;
 	uint length ;	
 	WDTCTL = WDTPW + WDTHOLD ;
 	BCSCTL1 = CALBC1_16MHZ ;
 	DCOCTL = CALDCO_16MHZ ;
+	mf522_spi.csPin = 3;
+	setup_uart_9600();
 	setupSpiSlave(&mf522_spi);	
 	MFRC522_Init(&mf522_spi);
 	while(1){
@@ -28,7 +32,9 @@ int main(){
 		if (status == MI_OK)
 		{
 			length = sprintf(printBuffer, "Card nb :%d%d \n",buffer[0], buffer[1] );                   
-		
+			uart_send_data((unsigned char*) printBuffer, length);
+		}else{
+			uart_send_data((unsigned char*) msg1, 17);
 		}
 		
 	}
--- myLib/inc/uart.h
+++ myLib/inc/uart.h
@@ -3,7 +3,7 @@
 #ifndef UART_H
 #define UART_H
 
-extern void uart_rx(void);
+extern void uart_rx(unsigned char byte);
 void uart_send_data(unsigned char * data, unsigned char length);
 void uart_send_char(unsigned char val);
 void setup_uart_9600();
