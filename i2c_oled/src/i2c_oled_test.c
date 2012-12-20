e51e3d91cae3840bc726c73240833cb64ccd328c
jpiat <jpiat@jpiat-laptop.(none)>
2012-12-20 20:13:36 +0100

    now working with hchar table

--- i2c_oled/src/i2c_oled_test.c
+++ i2c_oled/src/i2c_oled_test.c
@@ -1,49 +1,18 @@
 
 #include <msp430g2553.h>
 #include "my_types.h"
-//#include "i2c.h"
+#include "i2c.h"
+#include "CourierNew_5x7.h"
 #include <legacymsp430.h>
 
 unsigned char oledBuffer[2] ;
 unsigned char pageBuffer[] = {0xFF, 0x3F, 0x0F, 0x03, 0x00};
 
 
+
+
 //#define OLED_ADDR 0x7A
 #define OLED_ADDR 0x3D
-
-volatile unsigned char i2cBusy = 0;
-volatile unsigned char i2cBufferLength = 0 ;
-unsigned char * i2cBufferPtr ; 
-volatile char txDone = 1 ;
-
-void initi2c(){
-	
-	P1SEL |= BIT6 + BIT7 ;
-	P1SEL2 |= BIT6 + BIT7 ;
-	P1DIR |= BIT6 + BIT7;
-	UCB0CTL1 |= UCSWRST ;
-	UCB0CTL0 = UCMST + UCMODE_3 + UCSYNC ;
-	UCB0CTL1 = UCSSEL_2 + UCSWRST ;
-	UCB0BR0 = 192 ;
-	UCB0BR1 = 0;
-	UCB0I2CSA = OLED_ADDR ; 
-	UCB0I2CIE = UCNACKIE + UCALIE ;
-	UCB0CTL1 &= ~UCSWRST ;
-	IE2 |= UCB0TXIE | UCB0RXIE ;
-}
-void writei2c(unsigned char addr, unsigned char * data, unsigned char nbData){	
-	UCB0I2CSA = addr ;
-	i2cBusy = 0 ;
-	i2cBufferPtr = data ;
-	i2cBufferLength = nbData ;
-	txDone = 0 ;
-	UCB0CTL1 |= UCTR + UCTXSTT;
-	while(txDone == 0) ;
-}
-unsigned char readi2c(unsigned char addr){
-	return 0x00 ;
-}
-
 
 
 void oledSendData(unsigned char command){
@@ -142,46 +111,27 @@ int main(){
 	oledSendCommand(0x10);
 	oledSendCommand(0xAF);
 	P1OUT |= BIT0 ;
-	fill_oled_page(0, pageBuffer, 5);
-	fill_oled_page(1, pageBuffer, 5);
-	fill_oled_page(2, pageBuffer, 5);
-	fill_oled_page(3, pageBuffer, 5);
-	fill_oled_page(4, pageBuffer, 5);
-	fill_oled_page(5, pageBuffer, 5);
-	fill_oled_page(6, pageBuffer, 5);
-	fill_oled_page(7, pageBuffer, 5);
+		fill_oled_page(0, &data_table_SMALL[0], 128);
+	fill_oled_page(1, &data_table_SMALL[128], 128);
+	fill_oled_page(2, &data_table_SMALL[256], 128);
+	fill_oled_page(3, &data_table_SMALL[384], 128);
+	fill_oled_page(4, &data_table_SMALL[0], 128);
+	fill_oled_page(5, &data_table_SMALL[128], 128);
+	fill_oled_page(6, &data_table_SMALL[256], 128);
+	fill_oled_page(7, &data_table_SMALL[384], 128);
 	P1OUT &= ~BIT0 ;
 	while(1);
 }
 
 interrupt(USCIAB0TX_VECTOR) USCI0TX_ISR(void){
 	if (IFG2 & UCB0TXIFG) {	
-		if(i2cBusy >=  i2cBufferLength){
-			UCB0CTL1 |= UCTXSTP ;
-			i2cBusy = 0 ;	
-			txDone = 1 ;
-			IFG2 &= ~UCB0TXIFG;
-		}else{
-			UCB0TXBUF = i2cBufferPtr[i2cBusy] ;
-			i2cBusy ++ ;
-		}
+		i2cTxInterruptService();
 	}
 	
 }
 
 interrupt(USCIAB0RX_VECTOR) USCI0RX_ISR(void) {
-        if (UCB0STAT & UCNACKIFG) {
-		UCB0CTL1 |= UCTXSTP;
-		UCB0STAT &= ~UCNACKIFG;
-		i2cBusy = -1;
-		txDone = -1 ;
-	}else if (UCB0STAT & UCALIE) {
-                UCB0CTL1 |= UCTXSTP;
-                UCB0STAT &= ~UCALIE;
-                i2cBusy = -1;
-		txDone = -1 ;
-        }
-        
+      	i2cRxInterruptService();
 	IFG2 &= ~UCB0RXIFG;
 }
 
--- myLib/src/i2c.c
+++ myLib/src/i2c.c
@@ -8,22 +8,22 @@ unsigned char * i2cBufferPtr ;
 volatile char txDone = 1 ;
 
 void initi2c(){
+	
 	P1SEL |= BIT6 + BIT7 ;
 	P1SEL2 |= BIT6 + BIT7 ;
-	P1REN |= BIT6 + BIT7;
-	P1OUT |= BIT6 + BIT7 ;
+	P1DIR |= BIT6 + BIT7;
 	UCB0CTL1 |= UCSWRST ;
 	UCB0CTL0 = UCMST + UCMODE_3 + UCSYNC ;
 	UCB0CTL1 = UCSSEL_2 + UCSWRST ;
 	UCB0BR0 = 192 ;
 	UCB0BR1 = 0;
-	UCB0I2CSA = 0x3D ; 
+	UCB0I2CSA = 0x00 ; 
 	UCB0I2CIE = UCNACKIE + UCALIE ;
 	UCB0CTL1 &= ~UCSWRST ;
 	IE2 |= UCB0TXIE | UCB0RXIE ;
 }
 void writei2c(unsigned char addr, unsigned char * data, unsigned char nbData){	
-	UCB0I2CSA = 0x3C ;
+	UCB0I2CSA = addr ;
 	i2cBusy = 0 ;
 	i2cBufferPtr = data ;
 	i2cBufferLength = nbData ;
@@ -41,6 +41,7 @@ inline void i2cTxInterruptService(void){
 		UCB0CTL1 |= UCTXSTP ;
 		i2cBusy = 0 ;	
 		txDone = 1 ;
+		IFG2 &= ~UCB0TXIFG;
 	}else{
 		UCB0TXBUF = i2cBufferPtr[i2cBusy] ;
 		i2cBusy ++ ;
@@ -48,7 +49,7 @@ inline void i2cTxInterruptService(void){
 }
 
 inline void i2cRxInterruptService(void){
-	if (UCB0STAT & UCNACKIFG) {
+	  if (UCB0STAT & UCNACKIFG) {
 		UCB0CTL1 |= UCTXSTP;
 		UCB0STAT &= ~UCNACKIFG;
 		i2cBusy = -1;
