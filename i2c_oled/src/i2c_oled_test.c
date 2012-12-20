707c8951ddb27a9ad6b515e456cd0cd83fdf5e44
jpiat <jpiat@jpiat-laptop.(none)>
2012-12-21 00:37:24 +0100

    display different fonts

--- i2c_oled/src/i2c_oled_test.c
+++ i2c_oled/src/i2c_oled_test.c
@@ -3,13 +3,22 @@
 #include "my_types.h"
 #include "i2c.h"
 #include "CourierNew_5x7.h"
+#include "CourierNew_14x15.h"
 #include <legacymsp430.h>
 
 unsigned char oledBuffer[2] ;
 unsigned char pageBuffer[] = {0xFF, 0x3F, 0x0F, 0x03, 0x00};
+/*
+#define font_table data_table_SMALL
+#define FONT_WIDTH 5
+#define FONT_SPAN 1
+*/
+#define font_table data_table_LARGE
+#define FONT_WIDTH 14
+#define FONT_SPAN 2
 
-
-
+//const char * testPhrase = "CE QUI SE CONCOIT BIEN S'ENONCE CLAIREMENT ET LES MOTS POUR LE DIRE VOUS VIENNENT AISEMENT";
+const char * testPhrase = "THIS IS A TEST !";
 
 //#define OLED_ADDR 0x7A
 #define OLED_ADDR 0x3D
@@ -57,6 +66,42 @@ void Set_Start_Page(unsigned char d)
 						
 }
 
+void print_screen(unsigned char page, unsigned char column, unsigned char * txt){
+	unsigned int i = 0,j,k,c ;
+	unsigned int curPage = page, curCol=column ;
+	unsigned int cIndex ;
+	c = txt[i];	
+	while(c!='\0'){
+		cIndex = (c - 32)*FONT_WIDTH*FONT_SPAN;
+		for(k=0 ; k < FONT_SPAN ; k++){	
+			Set_Start_Page(curPage - k);
+			Set_Start_Column(curCol);
+			for(j=0 ; j < FONT_WIDTH ; j++){
+				oledSendData(font_table[cIndex + j + (k*FONT_WIDTH) ]);
+			}
+		}
+		i ++ ;
+		curCol = (curCol + (FONT_WIDTH));
+		if(curCol >= (128-FONT_WIDTH)){
+			curPage = curPage - FONT_SPAN ;
+			curCol = column ;
+		}
+		c = txt[i];
+	}
+}
+
+void fill_screen(unsigned char data)
+{
+   unsigned char i, j;
+	for(i = 0; i < 8 ; i ++){
+	   Set_Start_Page(i);
+	   Set_Start_Column(0x00);
+	   for(j=0;j<128;j++)
+	   {
+	      oledSendData(data);
+	   }
+	}
+}
 
 void fill_oled_page(unsigned char page, unsigned char * data, unsigned char dataSize)
 {
@@ -111,14 +156,16 @@ int main(){
 	oledSendCommand(0x10);
 	oledSendCommand(0xAF);
 	P1OUT |= BIT0 ;
-		fill_oled_page(0, &data_table_SMALL[0], 128);
+	fill_screen(0x00) ;
+	print_screen(5, 0, testPhrase);
+	/*	fill_oled_page(0, &data_table_SMALL[0], 128);
 	fill_oled_page(1, &data_table_SMALL[128], 128);
 	fill_oled_page(2, &data_table_SMALL[256], 128);
 	fill_oled_page(3, &data_table_SMALL[384], 128);
 	fill_oled_page(4, &data_table_SMALL[0], 128);
 	fill_oled_page(5, &data_table_SMALL[128], 128);
 	fill_oled_page(6, &data_table_SMALL[256], 128);
-	fill_oled_page(7, &data_table_SMALL[384], 128);
+	fill_oled_page(7, &data_table_SMALL[384], 128);*/
 	P1OUT &= ~BIT0 ;
 	while(1);
 }
